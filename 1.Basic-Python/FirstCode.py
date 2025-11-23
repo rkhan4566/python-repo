cd72cb6c82d4809f19331da6b63bb4879c48e05b
rkhan4566 <mdrehanalam4566@gmail.com>
2025-11-23 20:44:13 +0530

    save

--- 1.Basic-Python/FirstCode.py
+++ 1.Basic-Python/FirstCode.py
@@ -1,7 +1,23 @@
 print("Hello World")
 print("my first code ")
 print("i am rkhan10")
+#syntax
 a=1
 b=2
 sum=a+b
 print(sum)
+total=1+2+3+4+5+6+7+8
+print(total)
+a=1;b=2;x=a+b
+print(x)
+name="rehan"
+age=19
+print(type(name))
+print(type(age))
+##intentation
+age=20
+if age>19:
+    print("true")
+else:
+    print("false")
+##that is comment when we use double ## and ''' ''''
