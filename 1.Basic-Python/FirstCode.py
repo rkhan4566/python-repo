c6a0fe0388a5d2b3fcc142b577245460fb548522
rkhan4566 <mdrehanalam4566@gmail.com>
2025-11-27 20:39:18 +0530

    all loops

--- 1.Basic-Python/FirstCode.py
+++ 1.Basic-Python/FirstCode.py
@@ -1,3 +1,161 @@
+"""print("Hello World")
+print("my first code ")
+print("i am rkhan10")
+#syntax
+a=1
+b=2
+sum=a+b
+print(sum)
+total=1+2+3+4+5+6+7+8
+print(total)
+a=1;b=2;x=a+b
+print(x)
+name="rehan"
+age=19
+print(type(name))
+print(type(age))
+##indentation
+age=20
+if age>19:
+    print("true")
+else:
+    print("false")
+##that is comment when we use double ## and ''''''
+name="rehan"
+age=19
+height="5.8"
+print("name:",name)
+print("age:",age)
+print("height:",height)
+#type conversion
+age=19
+print(type(age))
+
+age_str=str(age)
+print(age_str)
+print(type(age_str))
+
+height=5.8
+print(int(height))
+print(float(height))
+
+ #dynamic type
+var=10
+print(var,type(var))
+var="rehan"
+print(var,type(var))
+var=6.0
+print(var,type(var))
+
+#input
+name=str(input("what is your name"))
+print(name,type(name))
+
+age=int(input("what is your age"))
+print=(age,type(age))
+
+#simple calculaator
+num1=float(input("entre your number"))
+num2=float(input("entre your number"))
+
+add=num1+num2
+difference=num1-num2
+product=num1*num2
+quotient=num1/num2
+
+print(add,"addition")
+print(difference,"diff")
+print(product,"mul")
+print(quotient,"divide")
+
+#integer data types
+age=32
+print(type(age))
+
+#float value data type
+height=5.8
+print(type(height))
+
+#string data types
+name="rehan"
+print(type(name))
+
+#boolean data types
+is_true=True
+print(type(is_true))
+
+a=10
+b=10
+print(type(a==b))
+
+a=Rkhan+str(10)
+print(a)
+
+#Arithmetic operation
+a=10
+b=5
+add_result=a+b #addition
+sub_result=a-b #substraction
+mul_result=a*b #multiplication
+div_result=a/b #division
+floor_div_result=a//b #floordivision
+modulus_result=a%b #modulus
+exponent_result=a**b #exponential
+
+print(add_result)
+print(sub_result)
+print(mul_result)
+print(div_result)
+print(floor_div_result)
+print(modulus_result)
+print(exponent_result)
+
+
+#comparison operator
+a=10
+b=10
+result=a==b
+print(result)
+
+
+#not equal value
+str1=("rehan")
+str2=("Rehan")
+result=str1!=str2
+print(result)
+
+#greater than less than and equal to
+a=5
+b=6
+result=a>b
+print(result)
+
+a=5
+b=4
+result=a>b
+print(result)
+
+a=6
+b=6
+result=a==b
+print(result)
+
+
+#AND OR and NOT logic operator
+a=True
+b=False
+result=a and b
+print(result)
+
+a=True
+b=False
+result=a or b
+print(result)
+
+a=True
+result= not a
+print(result)
+
 #simple calculator
 num1=float(input("entre your first number"))
 num2=float(input("entre your second number"))
@@ -73,3 +231,82 @@ else:
     result= "error division by zero"
 print(result)
 
+# for loop
+
+range(5)
+print(range)
+
+for i in range (5):
+    print(i)
+
+for i in range(1,10):
+    print(i)
+
+for i in range(1,10,2):
+    print(i)
+
+
+#while loop
+count=0
+while count<5:
+    count=count+1
+    print(count)
+
+count=0
+while count<100:
+    count=count+1
+    print(count)
+
+for i in range(10):
+   if i==5:
+    break
+   print(i)
+
+
+for i in range(100):
+   if i==25:
+      break
+   print(i)
+
+#pass null statement
+for i in range(5):
+   if i==4:
+    print("the null number",i)
+    pass
+   print( i)
+
+#nested loop
+#in loop inside a loop
+
+for i in range(5):
+  for j in range(4):
+    print(f"i:{i} and j:{j}")
+    
+n=10
+sum=0
+for i in range(11):
+   sum=sum+i
+print(sum)
+
+n=10
+sum=0
+count=1
+
+while count<=n:
+  sum=sum+count
+  count=count+1
+  print("sum of all natural number",sum)
+  
+"""
+#prime number of (1 , 100)
+
+for num in range(1,101):
+    if num>1:
+        for i in range(2,num):
+            if num%i==0:
+                break
+        else:
+            print(num)
+
+
+ 
