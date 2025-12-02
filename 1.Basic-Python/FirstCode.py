7ad04608433d5b795fe76d4a8bd8d8acb1cb7e45
rkhan4566 <mdrehanalam4566@gmail.com>
2025-12-02 17:48:03 +0530

    dictionaries

--- 1.Basic-Python/FirstCode.py
+++ 1.Basic-Python/FirstCode.py
@@ -61,3 +61,62 @@ print(student)
 student["name"]="rehan3"
 print(student)
 print(student_copy1)
+
+#itersting over dictionaries
+#you can use to loop to iterate over dictionaries,keys ,values or items
+
+#iterating over keys
+for keys in student.keys():
+    print(keys)
+
+#iterate over values
+for value in student.keys():
+    print(values)
+
+#iterating over key value pairs
+for keys,values in student.items():
+    print(f"{keys}:{values}")
+
+#nested dictionaries
+students={
+    "student1":{"name":"rehan","age":19,"height":5.8},
+    "student2":{"name":"sohail","age":19,"height":5.7}
+}
+print(students)
+
+#access nested dictionaries elements
+print(students["student2"]["age"])
+print(students["student2"]["height"])
+
+#iterating over nested dictionaries
+for student_id,student_info in students.items():
+    print(f"{student_id}:{student_info}")
+    for keys,values in student_info.items():
+        print(f"{keys}:{values}")
+
+#dictionaries comprehension
+student={x:x**2 for x in range(6)}
+print(student)
+
+#conditon dictionaries comprehension
+even={x:x**2 for x in range(10) if x%2==0}
+print(even)
+
+### SOME PRACTICAL EXAMPLES
+#use a dictionaries to count it frrequency of element in list
+
+numbers=[1,2,2,3,3,3,4,4,4,4]
+frequency={}
+
+for number in numbers:
+    if number in frequency:
+        frequency[number]+1
+    else:
+        frequency[number]=1
+print(frequency)
+
+#merge two dictionaries into one
+dict1={"a":1,"b":2}
+dict2={"a":3,"c":4}
+merge_dict={**dict1,**dict2}
+print(merge_dict)
