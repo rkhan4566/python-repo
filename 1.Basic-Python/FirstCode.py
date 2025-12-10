6f6b39c9f3633818102c6d77209eac88cd7bdcd2
rkhan4566 <mdrehanalam4566@gmail.com>
2025-12-10 15:06:40 +0530

    package and import method

--- 1.Basic-Python/FirstCode.py
+++ 1.Basic-Python/FirstCode.py
@@ -16,3 +16,15 @@ print(result)
 from math import *
 result1=sqrt(25)
 print(result1)
+
+from package.maths import addition
+result=addition(2,4)
+print(result)
+
+from package.maths import substraction
+result=substraction(4,2)
+print(result)
+
+from package.maths import multiplication
+result=multiplication(4,2)
+print(result)
