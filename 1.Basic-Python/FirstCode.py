2438e118ced6b866957b3cfaa5d767865303d19d
rkhan4566 <mdrehanalam4566@gmail.com>
2025-12-15 15:10:48 +0530

    classes and object

--- 1.Basic-Python/FirstCode.py
+++ 1.Basic-Python/FirstCode.py
@@ -1,82 +1,84 @@
-####  Understanding Exceptions  ###
-#Exception handling in Python allows you to handle errors gracefully and take corrective actions without stopping the execution of the program. This lesson will cover the basics of exceptions, including how to use try, except, else, and finally blocks.
+#classes and object in python
+#Object-Oriented Programming (OOP) is a programming paradigm that uses "objects" to design applications and computer programs. 
+#OOP allows for modeling real-world scenarios using classes and objects. This lesson covers the basics of creating classes and objects, including instance variables and methods.
 
-#What Are Exceptions?
-#Exceptions are events that disrupt the normal flow of a program. They occur when an error is encountered during program execution. Common exceptions include:
+###A class is a blueprint for creating objects.
+# It defines attributes (data) and methods (functions) that the objects will have.
 
-#  1.  ZeroDivisionError: Dividing by zero.
+"""class car:
+    pass
+audi=car()
+bmw=car()
+print(type(audi))
+print(audi)
 
-#  2.  FileNotFoundError: File not found.
+audi.windows=4
+print(audi.windows)
 
-#  3. ValueError: Invalid value.
+class car:
+    pass
+tata=car()
+print(tata)
 
-#  4. TypeError: Invalid type.
+tata.windows=5
+print(tata.windows)
+dir(tata)
 
-#try and except block
-try:
-    a=b
-except:
-    print("the variable has not be assigned")
+#insatnce variable and methods
+class dog:
+    #constructor
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    #create object
+dog1 = dog("buddy",3)
+print(dog1)
+print(dog1.name)
+print(dog1.age)
 
-try:
-    a=b
-except NameError as ex:
-    print(ex)
+dog2=dog("lucy",4)
+print(dog2.name)
+print(dog2.age)"""
 
-try:
-    result=1/0
-except ZeroDivisionError as ex:
-    print(ex)
-    print('entre the denominator is greater than zero')
+#define a class with instance methods
+class Dog:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def bark(self):
+        print(f"{self.name} says woof")
     
-try:
-    result=1/2
-    a=b
-except ZeroDivisionError as ex:
-    print(ex)
-except Exception as ex1:
-    print(ex1)
-    print('main exception get caught here')
+dog=Dog("lucy",3)
+dog.bark()
+
+#modelling a bank account
+
+#define a class of bank account
+class bankaccount:
+    def __init__(self,owner,balance=0):
+        self.owner=owner
+        self.balance=balance
+
+    def deposit(self,amount):
+        self.balance+=amount
+        print(f"{amount}is deposited.new balance is{self.balance}")
+
+    def withdraw(self,amount):
+        if amount>self.balance:
+            print("insufficient balance")
+        else:
+            self.balance-=amount
+            print(f"{amount}is withdrawn.new balance is {self.balance}")
+
+    def get_balance(self):
+        return self.balance
+    
+
+#create an account
+account=bankaccount("rehan",15000)
+print(account.balance)
+
+account.deposit(1000)
+account.withdraw(10000)
+
      
-#input method by try,except,else 
-try:
-    num=int(input("enter a number "))
-    result=10/num
-except ValueError:
-    print("this is not a valid error")
-except ZeroDivisionError:
-    print("entre denominator greater than zero")
-except Exception as ex:
-    print(ex)
-else:
-    print(f"the result is {result}")
-
-#try,except,else and finally blocks
-try:
-    num=int(input("enter a number "))
-    result=10/num
-except ValueError:
-    print("this is not a valid error")     
-except ZeroDivisionError:
-    print("you can't divided by zero")
-except Exception as ex:                     #except block is work when error will get found
-    print(ex)
-else:                                       #else is work when no error will be found
-    print(f"the result is {result}")  
-finally:
-    print("execution complete.")             #finally block works both in except and else 
-
-#file handling and exception handling
-try:
-    file=open('example.txt','r')
-    content=file.read()
-    a=b
-    print(content)
-except FileNotFoundError:
-    print("the file doesnot get exists")
-except Exception as ex:
-    print(ex)
-finally:
-    if 'file' in locals() and not file.closed():
-        file.close()
-        print('file close')
