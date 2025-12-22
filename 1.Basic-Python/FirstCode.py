8eae9f39061d4b3ea11779db6d23730f0f5fbf4f
rkhan4566 <mdrehanalam4566@gmail.com>
2025-12-22 21:32:18 +0530

    encapsulation

--- 1.Basic-Python/FirstCode.py
+++ 1.Basic-Python/FirstCode.py
@@ -1,93 +1,81 @@
-##polymorphism
+#Encapsulation And Abstraction
+#Encapsulation and abstraction are two fundamental principles of Object-Oriented Programming (OOP) that help in designing robust, maintainable, and reusable code. Encapsulation involves bundling data and methods that operate on the data within a single unit, while abstraction involves hiding complex implementation details and exposing only the necessary features.
 
-# polymorphism is a core concept in object oriented programming (oops) that allows objects of different classes 
-# to be treated as object of a common superclass it provode a way to perform a single action in diffeerent form.
-# polymorphism is typically achieved through method overriding and interfaces 
+#Encapsulation
+#Encapsulation is the concept of wrapping data (variables) and methods (functions) together as a single unit. It restricts direct access to some of the object’s components, which is a means of preventing accidental interference and misuse of the data.
 
-#base class 
-class animal:
-    def speak (self):
-        return "sound of animals"
-#derived class
-class dog(animal):
-    def speak(self):
-        return "woof"
+#encapsulation with getter and setter methods
+#public,protected,private variables
+class Person:
+    def __init__(self,name,age):
+        self.name=name  #public variable
+        self.age=age    #public variable
+
+def get_name(person):
+    return person.name
+
+person=Person("rehan",19)
+print(get_name(person))
+
+class Person:
+    def __init__(self,name,age,gender):
+        self.__name=name  #private variable
+        self.__age=age    #private variable
+        self.gender=gender
     
-class cat(animal): 
-    def speak(self):
-        return "meow"
+def get_name(person):
+    return person._Person__age
+
+person=Person("rehan",19,"male")
+print(get_name(person))
+
+
+class person:
+    def __init__(self,name,age,gender):
+        self._name=name
+        self._age=age
+        self._gender=gender
+
+class Employee(person):
+    def __init__(self,name,age,gender):
+        super().__init__(name,age,gender)
+
+employee=Employee("rehan",19,"male")
+print(employee._gender)
+
+#encapsulation with getter and setter method
+class Person:
+    def __init__(self,name,age):
+        self.__name=name #private access modifier or variable
+        self.__age=age   #private variable
+
+    #getter method for name
+    def get_name(self):
+        return self.__name
     
-#function that demonstrate polymorphism 
-def animal_speak(animal):
-    print(animal.speak(dog))
+    #setter method for name
+    def set_name(self,name):
+        self.__name=name
 
-dog1=dog()
-cat1=cat()
-print(dog1.speak())
-print(cat1.speak())
-animal_speak(dog)
-
-#polymorphism with function and methods
-# base class  
-class shape:
-    def area(self):
-        return "area of the figure"
+    #getter method for age
+    def get_age(self):
+        return self.__age
     
-#derived class
-class Rectangle(shape):
-    def __init__(self,width,height):
-        self.width=width
-        self.height=height
+    #setter methd for age
+    def set_age(self,age):
+        if age >0:
+            self.__age=age
+        else:
+            print("age cannot be negative.")
 
-    def area(self):
-        return self.width*self.height
-    
-class Circle(shape):
-    def __init__(self,radius):
-        self.radius=radius
+person=Person("rehan",19)
 
-    def area(self):
-        return 3.14*self.radius*self.radius
-    
-#functions that demonstrate polymorphism
+#access and modify private variable using getter and setter
+print(person.get_name())
+print(person.get_age())
 
-def print_area(shape):
-    print(f"the area is {shape.area()}")
+person.set_age(20)
+print(person.get_age())
 
-rectangle=Rectangle(4,5)
-circle=Circle(3)
-
-print_area(rectangle)
-print_area(circle)
-
-#Polymorphism with Abstract Base Classes
-#Abstract Base Classes (ABCs) are used to define common methods for a group of related objects. They can enforce that derived classes implement particular methods, promoting consistency across different implementations.
-from abc import ABC,abstractmethod
-
-#define a abstact class
-class Vehicle(ABC):
-    @abstractmethod
-    def start_engine(self):
-        pass
-
-#derive class 1
-class Car(Vehicle):
-    def start_engine(self):
-        return "car engine started"
-    
-#derived class 2
-class Motorcycle(Vehicle):
-    def start_engine(self):
-        return "motorcycle engine started"
-    
-#function that demonstrate polymorphism
-def start_vehicle(vehicle):
-    print(vehicle.start_engine())
-    
-#create an object of car and motorcycle
-car=Car()
-motorcycle=Motorcycle()
-
-start_vehicle(car)
-
-
+person.set_age(-5)
+        
