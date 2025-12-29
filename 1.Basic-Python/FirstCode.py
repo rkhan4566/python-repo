dfc3ee2474ecc24cfc6f7535d13a72b42e8988e2
rkhan4566 <mdrehanalam4566@gmail.com>
2025-12-29 19:56:38 +0530

    decorators

--- 1.Basic-Python/FirstCode.py
+++ 1.Basic-Python/FirstCode.py
@@ -1,4 +1,4 @@
-#Decorators:-
+"""#Decorators:-
 #Decorators are a powerful and flexible feature in Python that allows you to modify the behavior of a function or class method.
 # They are commonly used to add functionality to functions or methods without modifying their actual code.
 # This lesson covers the basics of decorators, including how to create and use them.
@@ -59,5 +59,32 @@ def main_welcome(func,lst):
     return sub_welcome_method()
 
 print(main_welcome(len,[1,2,3,4,5]))
-
+"""
 #decorators:-
+def my_decorator(func):
+    def wrapper():
+        print("something is happening before the function is called.")
+        func()
+        print("something is happening before the function is called.")
+    return wrapper 
+
+@my_decorator
+def say_hello():
+    print("hello")
+
+say_hello()
+
+#decorators with positional arguments:-
+def repeat(n):
+    def decorator(func):
+        def wrapper(*args,**kwargs):
+            for _ in range(n):
+                func(*args,**kwargs)
+        return wrapper
+    return decorator
+
+@repeat(5)
+def say_hello():
+    print("hello")
+
+say_hello()
