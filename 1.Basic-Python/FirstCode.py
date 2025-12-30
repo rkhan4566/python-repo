b82700d85a7b12e8c8ed39eb5998e107ebd6f69f
rkhan4566 <mdrehanalam4566@gmail.com>
2025-12-30 21:26:29 +0530

    numpy

--- 1.Basic-Python/FirstCode.py
+++ 1.Basic-Python/FirstCode.py
@@ -1,90 +1,137 @@
-"""#Decorators:-
-#Decorators are a powerful and flexible feature in Python that allows you to modify the behavior of a function or class method.
-# They are commonly used to add functionality to functions or methods without modifying their actual code.
-# This lesson covers the basics of decorators, including how to create and use them.
+#numpy:-NumPy is a fundamental library for scientific computing in Python.
+# It provides support for arrays and matrices, along with a collection of mathematical functions to operate on these data structures.
+# In this lesson, we will cover the basics of NumPy, focusing on arrays and vectorized operations.
 
-#Python
-## function copy
-## closures
-## decorators
+import numpy as np
+arr1=np.array([1,2,3,4,5])
+print(arr1)
+print(type(arr1))
+print(arr1.shape)
 
-#function copy:-
-def welcome():
-    return "welcome to advanced python course"
+arr2=np.array([1,2,3,4,5])
+arr2.reshape(1,5) #1row and 5 columns
+print(arr2)
 
-print(welcome())
+arr2=np.array([[1,2,3,4,5]])
+arr2.shape
+print(arr2.shape)
 
-wel=welcome()
-print(wel)
+#2d array
+import numpy as np
+arr2=np.array([[1,2,3,4,5],[2,3,4,5,6]])
+print(arr2)
+print(arr2.shape)
 
-del welcome
-print(wel)
+arr3=np.arange(0,10,2).reshape(5,1)
+print(arr3)
 
-#closers:-
-def main_welcome():
-    msg='welcome'
-    def sub_welcome_method():
-        print("welcome to the advanced python course")
-        print(msg)
-        print("please learn these concepts properly")
-    return sub_welcome_method()
+#create arrays
+arr4=np.ones((3,4))
+print(arr4)
 
-print(main_welcome())
+#identity matrix
+arr5=np.eye(3)
+print(arr5)
 
-#closers function
-def main_welcome(msg):
-   
-    def sub_welcome_method():
-        print("welcome to the advanced python course")
-        print(msg)
-        print("please learn these concepts properly")
-    return sub_welcome_method()
+import numpy as np
 
-print(main_welcome("welcome everyone"))
+arr = np.array([[1, 2, 3],[4, 5, 6]])
+#attributes of numpy array
+print("Array:\n", arr)
+print("Shape:", arr.shape)          # Output: (2, 3)
+print("Number of dimensions:", arr.ndim)   # Output: 2
+print("Size (number of elements):", arr.size)  # Output: 6
+print("Data type:", arr.dtype)      # Output: int64 (may vary based on platform)
+print("Item size (in bytes):", arr.itemsize)  # Output: 8 (may vary based on platform)
 
-def main_welcome(func):
-    def sub_welcome_method():
-        print("welcome to the advanced python course")
-        func("welcome everyone to this tutorial")
-        print("please learn these concepts properly")
-    return sub_welcome_method()
+#numpy vactorized operation
+arr1=np.array([1,2,3,4,5])
+arr2=np.array([10,20,30,40,50])
 
-main_welcome(print)
+#element wise addition
+print("addition:",arr1+arr2)
 
-def main_welcome(func,lst):
-    def sub_welcome_method():
-        print("welcome to the advanced python course")
-        print(func(lst))
-        print("please learn these concepts properly")
-    return sub_welcome_method()
+#element wise substraction
+print("substaction:",arr1-arr2)
 
-print(main_welcome(len,[1,2,3,4,5]))
-"""
-#decorators:-
-def my_decorator(func):
-    def wrapper():
-        print("something is happening before the function is called.")
-        func()
-        print("something is happening before the function is called.")
-    return wrapper 
+#element wise multiplication
+print("multiplication:",arr1*arr2)
 
-@my_decorator
-def say_hello():
-    print("hello")
+#element wise division
+print("division:",arr1/arr2)
 
-say_hello()
+#universal function
+import numpy as np
+arr=np.array([2,3,4,5,6])
+#square root
+print(np.sqrt(arr))
 
-#decorators with positional arguments:-
-def repeat(n):
-    def decorator(func):
-        def wrapper(*args,**kwargs):
-            for _ in range(n):
-                func(*args,**kwargs)
-        return wrapper
-    return decorator
+#exponential
+print(np.exp(arr))
 
-@repeat(5)
-def say_hello():
-    print("hello")
+#sine
+print(np.sin(arr))
 
-say_hello()
+#natural log
+print(np.log(arr))
+
+ #array slicing and indexing
+arr7=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+print("array:\n",arr7)
+
+print(arr7[1][1])
+print(arr7[1:])
+print(arr7[1:,2:])
+print(arr7[0:2,2:])
+
+arr7=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+print("array:\n",arr7)
+
+print(arr7[1:,1:3])
+
+#modify array element
+arr7[0,0]=100
+print(arr7)
+
+arr7[1:]=100
+print(arr7)
+
+#statistical concepts--normalization
+#to have a mean of 0 and standard deviation of 1
+data=np.array([1,2,3,4,5])
+
+#calculate the mean and standard deviation
+mean = np.mean(data)
+std_dev=np.std(data)
+
+#normalization of data
+normalized_data=(data-mean)/std_dev
+print("normalized data:",normalized_data)
+###
+data=np.array([1,2,3,4,5,6,7,8,9,10])
+#mean
+mean=np.mean(data)
+print("mean:",mean)
+
+#median
+median=np.median(data)
+print("median:",median)
+
+#standard deviation
+std_dev=np.std(data)
+print("standard deviation:",std_dev)
+
+#variance
+variance=np.var(data)
+print("variance:",variance)
+
+#logical data
+import numpy as np
+
+data=np.array([1,2,3,4,5,6,7,8,9])
+Data=data[(data>=5)&(data<=8)]
+print(Data)
+
+
+
+
