bf93daf54f56e486c4162d25d1840eaf8eadd98a
rkhan4566 <mdrehanalam4566@gmail.com>
2026-01-15 16:52:29 +0530

    complete Multithreading Practical Implementation With Python

--- 1.Basic-Python/FirstCode.py
+++ 1.Basic-Python/FirstCode.py
@@ -16,16 +16,30 @@ import threading
 import time
 def print_numbers():
     for i in range(5):
+        time.sleep(2)
         print(f"numbers:{i}")
 
-def print_latter():
-    for latter in "abcde":
-        print(f"Latter:{latter}")
+def print_letter():
+    for letter in "abcde":
+        time.sleep(2)
+        print(f"Letter:{letter}")
+
+## create two threads
+t1=threading.Thread(target=print_numbers)
+t2=threading.Thread(target=print_letter)
 
 t=time.time()
-print_numbers()
-print_latter() 
+# start the thread
+t1.start()
+t2.start()
+
+t=time.time() 
+#wait for the threads to complete
+t1.join()
+t2.join()
+
 finished_time=time.time()-t
-print(finished_time)      
+print(finished_time)
+
 
 
