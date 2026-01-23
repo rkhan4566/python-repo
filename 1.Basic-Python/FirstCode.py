2d3f6aa0d9db8840183a1cd7e2f5499301c4662c
rkhan4566 <mdrehanalam4566@gmail.com>
2026-01-23 20:53:32 +0530

    Working With HTTP Verbs Get And Post

--- 1.Basic-Python/FirstCode.py
+++ 1.Basic-Python/FirstCode.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 """it creates an instance of the flask class,
 which will be your WSGI (web server gateway interface)application.
 """
@@ -7,9 +7,17 @@ app=Flask(__name__)
 @app.route("/")
 def welcome():
     return "<html><H1>welcome to the flask course</H1></html>"
-@app.route("/index")
+@app.route("/index",methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/form',methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name=request.form['name']
+        return f'hello {name}!'
+    return render_template('form.html')
+
 
 if __name__=="__main__": 
     app.run(debug=True)
