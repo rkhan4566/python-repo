b37e70ac86b04a826be52430a7c4234db6209434
rkhan4566 <mdrehanalam4566@gmail.com>
2026-01-24 20:55:25 +0530

    Building Dynamic Url ,Variables Rule And Jinja 2 Template Engine

--- 1.Basic-Python/FirstCode.py
+++ 1.Basic-Python/FirstCode.py
@@ -4,19 +4,57 @@ which will be your WSGI (web server gateway interface)application.
 """
 #WSGI application
 app=Flask(__name__)
+
 @app.route("/")
 def welcome():
     return "<html><H1>welcome to the flask course</H1></html>"
+"""
 @app.route("/index",methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/form',methods=['GET','POST'])
-def form():
+@app.route('/submit',methods=['GET','POST'])
+def submit():
     if request.method=='POST':
         name=request.form['name']
         return f'hello {name}!'
     return render_template('form.html')
+"""
+## building url dinamically
+## variable rule
+## jinja 2 template engine
+'''
+{{  }} expression to print output in html
+{%..%} confitions,for loops
+{#..#} this is for comments
+'''
+
+#variable rule
+@app.route('/success/<int:score>')
+def success(score):
+    res=''
+    if score>=50:
+        res= "PASSED"
+    else:
+        "FAILED"
+    
+    return render_template('result.html',results=res)
+
+#variable rule
+@app.route('/successres/<int:score>')
+def successres(score):
+    res=''
+    if score>=50:
+        res= "PASSED"
+    else:
+        "FAILED"
+    
+    exp={'score':score,"res":res}
+    
+    return render_template('result1.html',results=exp)
+
+
+
 
 
 if __name__=="__main__": 
