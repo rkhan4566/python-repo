8ffb26ce4206d2d4fffdb475fb6571186d3ce1d8
rkhan4566 <mdrehanalam4566@gmail.com>
2026-01-26 19:48:22 +0530

    Working With Rest API's And HTTP Verbs Put And Delete

--- 1.Basic-Python/FirstCode.py
+++ 1.Basic-Python/FirstCode.py
@@ -1,92 +1,68 @@
-from flask import Flask,render_template,request,redirect,url_for
-"""it creates an instance of the flask class,
-which will be your WSGI (web server gateway interface)application.
-"""
-#WSGI application
-app=Flask(__name__)
+##put and delete
+## working with API's--json
 
-@app.route("/")
-def welcome():
-    return "<html><H1>welcome to the flask course</H1></html>"
-"""
-@app.route("/index",methods=['GET'])
-def index():
-    return render_template('index.html')
+from flask import Flask, jsonify,request
+app = Flask(__name__)
 
-@app.route('/submit',methods=['GET','POST'])
-def submit():
-    if request.method=='POST':
-        name=request.form['name']
-        return f'hello {name}!'
-    return render_template('form.html')
-"""
-## building url dinamically
-## variable rule
-## jinja 2 template engine
-'''
-{{  }} expression to print output in html
-{%..%} confitions,for loops
-{#..#} this is for comments
-'''
+##initial data in my to do list
+items= [
+    {"id": 1, "name": "item1", "description": "this is my item 1"},
+    {"id": 2, "name": "item2", "description": "this is my item 2"}
+]
 
-#variable rule
-@app.route('/success/<int:score>')
-def success(score):
-    res=''
-    if score>=50:
-        res= "PASSED"
-    else:
-        "FAILED"
-    
-    return render_template('result.html',results=res)
+@app.route('/')
+def home():
+    return "welcome to this sample to DO List App"
 
-#variable rule
-@app.route('/successres/<int:score>')
-def successres(score):
-    res=''
-    if score>=50:
-        res= "PASSED"
-    else:
-        "FAILED"
-    
-    exp={'score':score,"res":res}
-    
-    return render_template('result1.html',results=exp)
+#get and retreive all items 
+@app.route('/items',methods=['GET'])
+def get_items():
+    return jsonify(items)
 
-#if condition
-@app.route('/successif/<int:score>')
-def successif(score):
-    return render_template('result.html',results=score)
+## get: retireve a specific item by id
+@app.route('/items/<int:item_id>',methods=['GET'])
+def get_item(item_id):
+    item=next((item for item in items if item["id"]==item_id),None)
+    if item is None:
+        return jsonify({"error":"item not found"})
+    return jsonify(item)
 
-#dynamic
-@app.route('/fail/<int:score>')
-def fail(score):
-    
-    return render_template('result.html',results=score)
+#post :create a new task-API
+@app.route('/items',methods=['POST'])
+def create_item():
+    if not request.json or not 'name' in request.json:
+        return jsonify({"error": "items not found"})
+    new_item={
+         "id": items[-1]["id"] + 1 if items else 1,
+         "name":request.json['name'],
+         "description":request.json["description"]
 
-@app.route('/submit',methods=['POST,GET'])
-def submit():
-    total_score=0
-    if request.method=='POST':
-        science=float(request.form['science'])
-        maths=float(request.form['maths'])
-        c=float(request.form['c'])
-        data_science=float(request.form['datascience'])
+    }
+    items.append(new_item)
+    return jsonify(new_item)
 
-        total_score=(science+maths+c+data_science)/4
-    else:
-     return redirect(url_for('successres',score=int(total_score)))   
-    return render_template('getresult.html')
-      
+#put : update an existing item
+@app.route('/items<int:item_id>',methods=['PUT'])
+def update_item(item_id):
+    item =next((item for item in items if item["id"] == item_id),None)
+    if item is None:
+        return jsonify({"error": "item not found"})
+    item['name']= request.json.get('name',item['name'])
+    item['description']= request.json.get('description',item ['description'])
+    return jsonify(item)
+
+##DELETE: delete an item 
+@app.route('/items/<int:item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    global items
+    items= [ item for item in items if item["id"] !=item_id]
+    return jsonify({"result": "item deleted"})
 
 
-if __name__=="__main__": 
+
+
+if __name__=='__main__':
     app.run(debug=True)
-
-
-
-
-
 
 
 
